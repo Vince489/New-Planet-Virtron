9605fe4c567b402acf8b5e6217b9c6ca0e0e221c
Vince489 <vincentcornelius489@gmail.com>
2023-02-02 18:16:51 -0500

    yr

--- pages/fighters/index.vue
+++ pages/fighters/index.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="container mt-10 justify-center gap-6 flex flex-wrap">
-    <div v-for="g in gamers">
+    <div v-for="g in data">
       <Fighters :gamer="g" />
     </div>
   </div>
@@ -11,7 +11,7 @@
 <script setup>
 
 // fetch the gamer
-const { data: gamers } = await useFetch('https://interactive-production.up.railway.app/gamers')
+const { data } = await useFetch('https://interactive-production.up.railway.app/gamers')
 </script>
 
 <style scoped>
